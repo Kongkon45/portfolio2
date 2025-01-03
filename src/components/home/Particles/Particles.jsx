ec731c45bf45cefd24f2a42dd4852b898d31948b
Kongkon Jowarder <kongkon4545@gmail.com>
2025-01-03 17:41:16 +0600

    particles component redesing

--- src/components/home/Particles/Particles.jsx
+++ src/components/home/Particles/Particles.jsx
@@ -1,6 +1,121 @@
-"use client"
+// "use client"
+// import { useEffect, useState } from "react";
+// import Particles, { initParticlesEngine } from "@tsparticles/react";
+// import { loadFull } from "tsparticles";
+
+// const Particle = () => {
+//   const [init, setInit] = useState(false);
+
+//   useEffect(() => {
+//     const initParticles = async () => {
+//       console.log("Initializing Particles...");
+//       await initParticlesEngine(async (engine) => {
+//         await loadFull(engine);
+//       });
+//       setInit(true);
+//     };
+
+//     initParticles();
+//   }, []);
+
+//   const particlesLoaded = (container) => {
+//     console.log("Particles Loaded", container);
+//   };
+
+//   return (
+//     <>
+//       {init && (
+//         <Particles
+//           id="tsparticles"
+//           particlesLoaded={particlesLoaded}
+//           style={{
+//             zIndex: -1,
+//           }}
+//           options={{
+//             background: {
+//               color: {
+//                 value: "#fff",
+//               },
+//             },
+//             fpsLimit: 120,
+//             interactivity: {
+//               events: {
+//                 onClick: {
+//                   enable: true,
+//                   mode: "push",
+//                 },
+//                 onHover: {
+//                   enable: true,
+//                   mode: "repulse",
+//                 },
+//                 resize: true,
+//               },
+//               modes: {
+//                 push: {
+//                   quantity: 4,
+//                 },
+//                 repulse: {
+//                   distance: 200,
+//                   duration: 0.4,
+//                 },
+//               },
+//             },
+//             particles: {
+//               color: {
+//                 value: "#ed2218",
+//               },
+//               links: {
+//                 color: "#f7655e",
+//                 distance: 150,
+//                 enable: true,
+//                 opacity: 0.5,
+//                 width: 1,
+//               },
+//               move: {
+//                 direction: "none",
+//                 enable: true,
+//                 outModes: {
+//                   default: "bounce",
+//                 },
+//                 random: false,
+//                 speed: 1.2,
+//                 straight: false,
+//               },
+//               number: {
+//                 density: {
+//                   enable: true,
+//                   area: 800,
+//                 },
+//                 value: 160,
+//               },
+//               opacity: {
+//                 value: 0.5,
+//               },
+//               shape: {
+//                 type: "circle",
+//               },
+//               size: {
+//                 value: { min: 1, max: 5 },
+//               },
+//             },
+//             detectRetina: true,
+//           }}
+//         />
+//       )}
+//     </>
+//   );
+// };
+
+// export default Particle;
+
+
+"use client";
+
 import { useEffect, useState } from "react";
-import Particles, { initParticlesEngine } from "@tsparticles/react";
+import dynamic from "next/dynamic";
+
+// Dynamically import the Particles component to ensure it's only loaded on the client side
+const Particles = dynamic(() => import("@tsparticles/react"), { ssr: false });
 import { loadFull } from "tsparticles";
 
 const Particle = () => {
@@ -9,6 +124,7 @@ const Particle = () => {
   useEffect(() => {
     const initParticles = async () => {
       console.log("Initializing Particles...");
+      const { initParticlesEngine } = await import("@tsparticles/react");
       await initParticlesEngine(async (engine) => {
         await loadFull(engine);
       });
@@ -107,4 +223,3 @@ const Particle = () => {
 };
 
 export default Particle;
-
