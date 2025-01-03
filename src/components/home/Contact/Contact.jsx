ad8ceecff7fa657b9f701bb2421b571b62ee787c
Kongkon Jowarder <kongkon4545@gmail.com>
2025-01-03 17:51:14 +0600

    contact

--- src/components/home/Contact/Contact.jsx
+++ src/components/home/Contact/Contact.jsx
@@ -1,5 +1,6 @@
 "use client";
-import Lottie from "lottie-react";
+// import Lottie from "lottie-react";
+const Lottie = dynamic(() => import('react-lottie'), { ssr: false });
 import React from "react";
 import { useForm } from "react-hook-form";
 import contactData from "../../../../public/assets/images/contact.json";
